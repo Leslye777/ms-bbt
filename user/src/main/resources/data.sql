b12bacdde77c0eb5d1cde9bd56f19b9dc37bcd3d
Leslye <leslimdf@hotmail.com>
2023-06-10 11:36:09 -0300

    brcrypt

--- user/src/main/resources/data.sql
+++ user/src/main/resources/data.sql
@@ -1,19 +1,19 @@
 -- Insert User 1
 INSERT INTO users (first_name, last_name, email, password, role)
-VALUES ('John', 'Doe', 'johndoe@email.com', 'password123', 'USER');
+VALUES ('John', 'Doe', 'johndoe@email.com', '$2a$10$tUTO3pnvWOvzcD2BuPVhyO4ut/yzT7GBmyNX39q7ewBBHYiM2Fnoe', 'USER');
 
 -- Insert User 2
 INSERT INTO users (first_name, last_name, email, password, role)
-VALUES ('Jane', 'Doe', 'janedoe@email.com', 'password123', 'USER');
+VALUES ('Jane', 'Doe', 'janedoe@email.com', '$2a$10$tUTO3pnvWOvzcD2BuPVhyO4ut/yzT7GBmyNX39q7ewBBHYiM2Fnoe', 'USER');
 
 -- Insert User 3
 INSERT INTO users (first_name, last_name, email, password, role)
-VALUES ('Bob', 'Smith', 'bobsmith@email.com', 'password123', 'OPERATOR');
+VALUES ('Bob', 'Smith', 'bobsmith@email.com', '$2a$10$tUTO3pnvWOvzcD2BuPVhyO4ut/yzT7GBmyNX39q7ewBBHYiM2Fnoe', 'OPERATOR');
 
 -- Insert User 4
 INSERT INTO users (first_name, last_name, email, password, role)
-VALUES ('Alice', 'Smith', 'alicesmith@email.com', 'password123', 'OPERATOR');
+VALUES ('Alice', 'Smith', 'alicesmith@email.com', '$2a$10$tUTO3pnvWOvzcD2BuPVhyO4ut/yzT7GBmyNX39q7ewBBHYiM2Fnoe', 'OPERATOR');
 
 -- Insert User 5
 INSERT INTO users (first_name, last_name, email, password, role)
-VALUES ('Admin', 'User', 'admin@email.com', 'password123', 'ADMIN');
+VALUES ('Admin', 'User', 'admin@email.com', '$2a$10$tUTO3pnvWOvzcD2BuPVhyO4ut/yzT7GBmyNX39q7ewBBHYiM2Fnoe', 'ADMIN');
